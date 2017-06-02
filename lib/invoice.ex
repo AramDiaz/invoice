861e882c8ab5c161a0e837f9e64c991319fb4fb1
Aram Diaz <samuzaramsdc@gmail.com>
2017-06-02 13:11:42 -0500

    commits

---
+++ lib/invoice.ex
@@ -0,0 +1,18 @@
+defmodule INVOICE do
+  @moduledoc """
+  Documentation for INVOICE.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> INVOICE.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/invoice_test.exs
@@ -0,0 +1,8 @@
+defmodule INVOICETest do
+  use ExUnit.Case
+  doctest INVOICE
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
