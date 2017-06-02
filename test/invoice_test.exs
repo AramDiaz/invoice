6c84ec5ca16245febde2f6b27b3d2b1c4aadb8c5
Aram Diaz <samuzaramsdc@gmail.com>
2017-06-02 15:14:34 -0500

    Commit Test

--- test/invoice_test.exs
+++ test/invoice_test.exs
@@ -2,6 +2,7 @@ defmodule INVOICETest do
   use ExUnit.Case
   doctest INVOICE
 
+  #Test 1 request
   test "RequestApi" do
     id = "9918808d-8c8f-4b2e-8d7f-d53b89395f45"
     start = Date.from_iso8601!("2017-05-01")
@@ -9,6 +10,7 @@ defmodule INVOICETest do
     assert InvoiceAPIImp.getInvoice(id, start, finish)
   end
 
+  #Test todo el algoritmo completo
   test "AlgoritmoQueryInvoices" do
     id = "9918808d-8c8f-4b2e-8d7f-d53b89395f45"
     start = Date.from_iso8601!("2017-05-01")
