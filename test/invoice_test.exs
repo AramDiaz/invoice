beef6fa481029308a3f5779ae570a7405b27d9a7
Aram Diaz <samuzaramsdc@gmail.com>
2017-06-02 15:06:16 -0500

    Commit

--- test/invoice_test.exs
+++ test/invoice_test.exs
@@ -2,17 +2,18 @@ defmodule INVOICETest do
   use ExUnit.Case
   doctest INVOICE
 
-  test "QueryInvoices" do
-    id = "9918808d-8c8f-4b2e-8d7f-d53b89395f45"
-    start = Date.from_iso8601!("2017-05-01")
-    finish = Date.from_iso8601!("2017-05-30")
-    assert QueryInvoices.start(id, start, finish)
-  end
-
   test "RequestApi" do
     id = "9918808d-8c8f-4b2e-8d7f-d53b89395f45"
     start = Date.from_iso8601!("2017-05-01")
     finish = Date.from_iso8601!("2017-05-30")
     assert InvoiceAPIImp.getInvoice(id, start, finish)
   end
+
+  test "AlgoritmoQueryInvoices" do
+    id = "9918808d-8c8f-4b2e-8d7f-d53b89395f45"
+    start = Date.from_iso8601!("2017-05-01")
+    finish = Date.from_iso8601!("2017-05-30")
+    assert QueryInvoices.start(id, start, finish)
+  end
+
 end
